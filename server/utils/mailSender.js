3d32f067b7380f86a810465cc634a7852dd39336
M_NAMEER_K <nameerkhan0123@gmail.com>
2025-10-06 13:49:34 +0530

    Update StudyNotion project with latest changes

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -4,9 +4,12 @@ exports.mailSender = async (email,title,body) =>{
     try{
            let transporter =nodemailer.createTransport({
             host : process.env.MAIL_HOST,
+            port: 465,
+                  secure: true, 
             auth :{
                 user:process.env.MAIL_USER,
-                pass:process.env.MAIL_PASS
+                pass:process.env.MAIL_PASS,
+                   
             }
            });
            let info = await transporter.sendMail({
